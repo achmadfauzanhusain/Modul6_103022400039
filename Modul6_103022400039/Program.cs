abc32b5da8db90ef0eae9f2f3874a6049153687e
fauzanchenko <79782055+achmadfauzanhusain@users.noreply.github.com>
2026-04-07 07:39:21 +0700

    commit message
    commit message

--- Modul6_103022400039/Program.cs
+++ Modul6_103022400039/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaMusicTrack
+{
+    private int id;
+    private int playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        this.title = title;
+        this.playCount = 0;
+
+        Random rand = new Random();
+        this.id = rand.Next(10000, 99999);
+    }
+
+    public void increasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void printTrackDetails()
+    {
+        Console.WriteLine("=== Track Details ===");
+        Console.WriteLine("ID         : " + id);
+        Console.WriteLine("Title      : " + title);
+        Console.WriteLine("PlayCount  : " + playCount);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaMusicTrack track = new SayaMusicTrack("Lagu Favorit");
+        track.increasePlayCount(5);
+        track.printTrackDetails();
+    }
+}
